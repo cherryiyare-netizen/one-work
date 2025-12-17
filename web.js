37f3118981d0546687e26509d85aee587f80c8f9
cherryiyare-netizen <cherryiyare@gmail.com>
2025-12-17 05:54:38 +0100

    fourth commit

--- web.js
+++ web.js
@@ -1,10 +1,36 @@
 var client = {
     fullname : "Fendi James",
     height : 160,
-    age : 30,
+    age : 2025,
     complexion : "fair"
 
 
 };
-console.log(client.age);
+console.log(client.height);
 console.log(client.fullname);
+{
+    client.age -= 1995;
+    client.complexion += "skin type";
+}
+console.log(client.age);
+console.log(client.complexion);
+
+function curve (a, b, c)
+{var  total = (a + b + c);
+    return total;
+}
+var result = curve(2, 3.142, 4);
+console.log(result)
+
+function bed (i, j, k)
+{
+    var size = (i + j - k);
+    return size;
+}
+var bedsize = bed(3, 7, 2);
+console.log(bedsize)
+{
+    const g = 4;
+    var harp = (g + 10);
+}
+console.log(harp)
